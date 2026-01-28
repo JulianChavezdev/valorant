8c9478998bea81b2c373cc3deb429ea9cb334a9b
Julian Chavez <julicgavez1@gmail.com>
2026-01-28 14:26:20 +0100

    fix

--- src/valorantAgentes/Agente.java
+++ src/valorantAgentes/Agente.java
@@ -1,6 +1,18 @@
+package valorantAgentes;
+
 public class Agente {
     private String nombre;
-    private
+    private String pais;
+    private int vidaBase;
+    private boolean estaVivo;
 
+    public Agente(String nombre, String pais, int vidaBase, boolean estaVivo) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.vidaBase = vidaBase;
+        this.estaVivo = estaVivo;
+    }
 
+    public Agente() {
+    }
 }
