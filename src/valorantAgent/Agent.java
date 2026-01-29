c498f299edda13f57173635035f3c85ab8bc8334
Julian Chavez <julicgavez1@gmail.com>
2026-01-29 11:25:29 +0100

    adding Skill class

---
+++ src/valorantAgent/Agent.java
@@ -0,0 +1,75 @@
+package valorantAgent;
+
+import java.util.Objects;
+
+public class Agent {
+    private String name;
+    private String country;
+    private int baseLife;
+    private boolean Itslive;
+
+    public Agent(String name, String country, int baseLife, boolean itslive) {
+        this.name = name;
+        this.country = country;
+        this.baseLife = baseLife;
+        Itslive = itslive;
+    }
+
+    public Agent() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getBaseLife() {
+        return baseLife;
+    }
+
+    public void setBaseLife(int baseLife) {
+        this.baseLife = baseLife;
+    }
+
+    public boolean isItslive() {
+        return Itslive;
+    }
+
+    public void setItslive(boolean itslive) {
+        Itslive = itslive;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Agent agent = (Agent) o;
+        return baseLife == agent.baseLife && Itslive == agent.Itslive && Objects.equals(name, agent.name) && Objects.equals(country, agent.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, country, baseLife, Itslive);
+    }
+
+    @Override
+    public String toString() {
+        return "Agent{" +
+                "name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                ", baseLife=" + baseLife +
+                ", Itslive=" + Itslive +
+                '}';
+    }
+}
+
---
+++ src/valorantAgent/Skill.java
@@ -0,0 +1,50 @@
+package valorantAgent;
+
+import java.util.Objects;
+
+public class Skill extends Agent{
+    private String name;
+    private int price;
+    private boolean ulti;
+    private enum Efect{DAÑO,CURACION,CEGUERA};
+    private Efect efect;
+
+    public Skill(String name, String country, int baseLife, boolean itslive, String name1, int price, boolean ulti, Efect efect) {
+        super(name, country, baseLife, itslive);
+        this.name = name1;
+        this.price = price;
+        this.ulti = ulti;
+        this.efect = efect;
+    }
+
+    public Skill(String name, int price, boolean ulti, Efect efect) {
+        this.name = name;
+        this.price = price;
+        this.ulti = ulti;
+        this.efect = efect;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Skill skill = (Skill) o;
+        return price == skill.price && ulti == skill.ulti && Objects.equals(name, skill.name) && efect == skill.efect;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name, price, ulti, efect);
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", ulti=" + ulti +
+                ", efect=" + efect +
+                '}';
+    }
+}
+
