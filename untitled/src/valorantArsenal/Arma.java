64cfb8b1715d96433d14c010ec997dfc6edaeffe
Mito-05 <manuelvasco05@gmail.com>
2026-01-29 10:43:56 +0100

    Added Arma class

--- untitled/src/valorantArsenal/Arma.java
+++ untitled/src/valorantArsenal/Arma.java
@@ -1,4 +1,65 @@
 package valorantArsenal;
 
+import java.util.Objects;
+
 public class Arma {
+    private String name;
+    private double price;
+    private boolean itsAutomatic;
+    protected enum Category {PISTOLA, SUBFUSIL, FUSIL, ESCOPETA, FRANCOTIRADOR, AMETRALLADORA}
+    private Category category;
+
+    public Arma(String name, double price, boolean itsAutomatic, Category category) {
+        this.name = name;
+        this.price = price;
+        this.itsAutomatic = itsAutomatic;
+        this.category = category;
+    }
+
+    public Arma() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isItsAutomatic() {
+        return itsAutomatic;
+    }
+
+    public void setItsAutomatic(boolean itsAutomatic) {
+        this.itsAutomatic = itsAutomatic;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Arma arma = (Arma) o;
+        return Double.compare(price, arma.price) == 0 && itsAutomatic == arma.itsAutomatic && Objects.equals(name, arma.name) && category == arma.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, itsAutomatic, category);
+    }
 }
