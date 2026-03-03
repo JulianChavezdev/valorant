639278d6f41908a72151d03d872c357e874ecc13
Julian Chavez <julicgavez1@gmail.com>
2026-03-03 09:46:30 +0100

    fix

--- src/maps/SiteMap.java
+++ src/maps/SiteMap.java
@@ -1,13 +1,16 @@
+import java.util.Map;
 
-public class Statistic {
+public class SiteMap extends Map {
     private int kills;
     private int deaths;
     private int assists;
     private double combatScore;
 
-    public Statistic() {}
 
-    public Statistic(int kills, int deaths, int assists, double combatScore) {
+    public SiteMap(int kills, int deaths, int assists) {
+    }
+
+    public SiteMap(int kills, int deaths, int assists, double combatScore) {
         this.kills = kills;
         this.deaths = deaths;
         this.assists = assists;
