9f51187199643988a741398ac7e540b3c6d01ddc
Julian Chavez <julicgavez1@gmail.com>
2026-03-08 15:01:38 +0100

    adding comments

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
+// OJO CON ESTO INICILIZAMOS EL FRAME, lo puse en un package aparte para no tener la logica de la interfaz dentro del main
 
         SwingUtilities.invokeLater(() -> new ShopWindow());
     }
