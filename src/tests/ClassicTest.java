e60f79999b68fa0531e82453e022f283b7eeb115
Mito-05 <manuelvasco05@gmail.com>
2026-03-09 10:26:06 +0100

    Modify Agent constructor

--- src/tests/ClassicTest.java
+++ src/tests/ClassicTest.java
@@ -1,9 +1,12 @@
 package tests;
 
+import valorantAgent.Agent;
+
 class ClassicTest {
 
     @org.junit.jupiter.api.Test
     void shootLc() {
+        Agent jett = new Agent("Jett", "Korea",100,true);
 
     }
 
